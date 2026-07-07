f6cf3bb4270703cdb4a6ef1d211e87c80322ea7f
Darwin D Wu <darwin67@users.noreply.github.com>
2026-07-07 13:04:50 -0700

    update nix version to use 26.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "System flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -148,7 +148,7 @@
             users.workstation-username =
               { pkgs, ... }:
               {
-                home.stateVersion = "25.11";
+                home.stateVersion = "26.05";
 
                 # Enable direnv
                 programs.direnv = {
